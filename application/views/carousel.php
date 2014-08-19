276ad919b38e75c10dc376feef5067125e847756
JerryR7 <vp780412@gmail.com>
2014-08-19 15:01:05 +0800

    Update carousel.php and add a new image
    Add some texts about work remotely and change a new photo.

--- application/views/carousel.php
+++ application/views/carousel.php
@@ -1,11 +1,27 @@
-<div class="carousel slide" id="carousel-848635">
+<div style="padding-top: 15px">
+  <div style="padding: 60px 30px 30px 30px; margin-bottom: 30px; font-size: 21px; font-weight: 200; line-height: 2.1428571435; color: inherit; position: absolute; z-index: 10;">
+<!--     <h1>
+      Hello, world!
+    </h1> -->
+    <h3><strong>作為雇主</strong></h3>
+    <p>
+      只聘用一個較小地理區域的求職者，代表你無法得到最優秀的人才。
+    </p>
+    <h3><strong>作為員工</strong></h3>
+    <p>
+      只尋找合理通勤範圍內的公司，表示你服務的公司對你而言並非最好。
+    </p>
+    <p>
+      <?php echo anchor('pages/about', 'Learn more', 'class="btn btn-warning btn-large"');?>
+    </p>
+  </div>
   <div class="carousel-inner">
     <div class="item active">
-      <img src="<?php echo base_url();?>img/header.jpg" alt="...">
-      <div class="carousel-caption">
-<!--         <h3>...</h3>
-        <p>...</p> -->
-      </div>
+      <img src="<?php echo base_url();?>img/remote-work-1.jpg" alt="...">
+<!--       <div class="carousel-caption">
+        <h3>...</h3>
+        <p>...</p>
+      </div> -->
     </div>
   </div> 
 </div>
